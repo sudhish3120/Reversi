811bf2564584cf00e7c5d737013984b7f6a83d90
Sudhish <sudhish.3120@gmail.com>
2020-06-11 21:41:32 -0400

    Initial Commit

---
+++ Reversi AI/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Reversi AI
+//
+//  Created by Sudhish M on 2020-06-11.
+//  Copyright © 2020 Sudhish M. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
